0b6d21749115848f57fae65aacc1e6532d999c3c
Adam Pinsky <adam.pinsky4@gmail.com>
2016-02-14 15:19:59 -0600

    complete 4.5.1 and 4.5.2

---
+++ week-4/4.5-flow-control/4.5.2-calculate-a-letter-grade/my_solution.rb
@@ -0,0 +1,21 @@
+# Calculate a Grade
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def get_grade(test_grade)
+	if test_grade >= 90
+		return 'A'
+	elsif test_grade >= 80
+		return 'B'
+	elsif test_grade >= 70
+		return 'C'
+	elsif test_grade >= 60
+		return 'D'
+	else
+		return 'F'
+	end
+end
+		
